9f6aca2b6f7eba978509b50c576fa3ae26088a18
Valentin Squirelo <valentin.squirelo@gmail.com>
2021-10-08 15:28:17 +0200

    Revert "add config"
    This reverts commit 30e8cc2fed8a77078b2f4821b384dad858308cb4.

--- config.example.js
+++ config.example.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x3b85baf0e2157183c68d7143cddacb4eed75e0a1"
-export const nftaddress = "0xed0d22db288e029e54f9c5259338fc7bd6295fb4"
+export const nftmarketaddress = ""
+export const nftaddress = ""
